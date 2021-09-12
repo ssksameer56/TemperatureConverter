01383a5ddb402c6a38cda161a53d435b750352cc
Sameer Kolpekwar <ssksameer.56@gmail.com>
2021-09-12 20:12:38 +0530

    command line arguments

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	initialTemp := flag.Float64("temp", 0.0, "The temperature to convert")
+	originUnit := flag.String("unit", "C", "The unit of temperature")
+
+	flag.Parse()
+
+	var temp float64
+	if *originUnit == "C" {
+		temp = ConvertToCelsius(*initialTemp)
+	} else {
+		temp = ConvertToFarhenheit(*initialTemp)
+	}
+	fmt.Println("New Temperature is ", temp)
+}
+
+func ConvertToCelsius(value float64) float64 {
+	fmt.Println("Converting to Celsuis", value)
+	convertedValue := (value - 32) * 5 / 9
+	return convertedValue
+}
+
+func ConvertToFarhenheit(value float64) float64 {
+	fmt.Println("Converting to Farhenheit", value)
+	convertedValue := (value * 9 / 5) + 32
+	return convertedValue
 }
